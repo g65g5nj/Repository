8572a45dd7694c06938a6b53e809de76d22140be
WuChangkun <979421572@qq.com>
2021-09-26 22:53:13 +0800

    [update] fix all repository outputs

--- build/fix_aar_cache.py
+++ build/fix_aar_cache.py
@@ -13,13 +13,16 @@ CACHE_DIR="{0}/caches/modules-2/files-2.1/".format(SRC_DIR)
 URLS=[
     "http://bx-mvn.dataverse.cn:58081/repository/maven-releases/",
     "http://maven.aliyun.com/nexus/content/repositories/jcenter/",
+    "http://maven.aliyun.com/nexus/content/repositories/google/",
     'http://developer.huawei.com/repo/',
-    "https://s3.amazonaws.com/moat-sdk-builds",
-    "https://adcolony.bintray.com/AdColony",
-    'https://jitpack.io',
-    'https://chartboostmobile.bintray.com/Chartboost',
-    'https://artifact.bytedance.com/repository/pangle',
-    'https://android-sdk.is.com/'
+    "https://s3.amazonaws.com/moat-sdk-builds/",
+    "https://adcolony.bintray.com/AdColony/",
+    'https://jitpack.io/',
+    'https://chartboostmobile.bintray.com/Chartboost/',
+    'https://artifact.bytedance.com/repository/pangle/',
+    'https://android-sdk.is.com/',
+
+    "https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea/"
 ]
 
 process = subprocess.Popen(args=[
@@ -36,25 +39,27 @@ for i in range(len(result)):
     LOCAL_POM = filename.replace(".aar",".pom")
     LOCAL_POM = LOCAL_POM.replace("\n","")
     LOCAL_POM_DST = LOCAL_POM
-    LOCAL_POM = LOCAL_POM[len(SRC_DIR)+1:]
 
-    END_URL = LOCAL_POM
     REAL_EXIST = False
     if os.path.exists(LOCAL_POM):
         with open(LOCAL_POM) as f:
             texts = f.read()
+            f.close()
             if len(texts) == 0:
-                pass
+                os.remove(LOCAL_POM)
                 # print("{0} is empty".format(LOCAL_POM))
             else:
                 REAL_EXIST = True
                 # print("{0} is real".format(LOCAL_POM))
-            f.close()
     else:
         pass
         # print("{0} is not exists".format(LOCAL_POM))
     if REAL_EXIST:
         continue
+
+    LOCAL_POM = LOCAL_POM[len(SRC_DIR)+1:]
+    END_URL = LOCAL_POM
+
     FILE = LOCAL_POM[LOCAL_POM.rfind("/")+1:]
     LOCAL_POM = LOCAL_POM[:LOCAL_POM.rfind("/")]
 
@@ -66,18 +71,22 @@ for i in range(len(result)):
     ORG = LOCAL_POM[:LOCAL_POM.rfind("/")]
     ORG = ORG.replace("/",".")
     # print("ORG : {0}, FILE : {1}, REVISION : {2}".format(ORG,FILE,REVISION))
-
+    requireSuccess = False
     for i in range(len(URLS)):
         host = URLS[i]
         fullUrl = host + END_URL
         # print("fullUrl : {0} LOCAL_POM_DST : {1}".format(fullUrl,LOCAL_POM_DST))
         try:
-            response = requests.get(url=fullUrl)
+            response = requests.get(url=fullUrl,timeout=5)
             response.raise_for_status()
             with open(LOCAL_POM_DST,mode="w+") as f:
                 print(response.text)
                 f.write(response.text)
                 f.close()
+                requireSuccess = True
+                break
         except Exception as e:
             # print(e)
             pass
+    if not requireSuccess:
+        print("Failed -------  ORG : {0}, FILE : {1}, REVISION : {2}".format(ORG,FILE,REVISION))
